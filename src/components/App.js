cefe0feb73cb70843f2fa9e06d2e10f0e0eec67e
ArtZBr <34827135+ArtZBr@users.noreply.github.com>
2021-02-03 00:53:36 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            DApp Start Kit
           </a>
         </nav>
         <div className="container-fluid mt-5">
