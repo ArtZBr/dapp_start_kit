feb4139a9c14b1aaf57a8cabc1b2e5a5fd6aaf58
ArtZBr <34827135+ArtZBr@users.noreply.github.com>
2021-02-03 22:21:12 -0300

    Create Principal.js

---
+++ src/contracts/Principal.js
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+// Change the contract name as you wish
+// Don't forget to rename the file .sol
+
+contract Principal {
+	string public name;
+
+	constructor() public {
+		name = "Principal";
+	}
+}
